2a9addb53808a6935dd027d5a41f1c3b330f0376
Jason Wang <jwmo@students.cs.ubc.ca>
2025-02-13 23:12:03 -0800

    Changed ReadCSV code and TestCSVFile, as they were made referencing a different CSV file Workday also generates. They now use the correct CSV file.

--- src/main/model/Calendar.java
+++ src/main/model/Calendar.java
@@ -1,12 +1,10 @@
 package model;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 //Represents a Calendar with a name
 public class Calendar {
@@ -25,30 +23,30 @@ public class Calendar {
         daysOfWeek.add(new Day(DayOfWeek.FRIDAY));
     }
 
-    //MOFDIFIES: this
-    //REQUIRES: The file path must lead to a CSV of registered courses as created by Workday
-    //EFFECTS: Convert CSV file into a list of courses and adds courses to Calendar
+    // MOFDIFIES: this
+    // REQUIRES: The file path must lead to a CSV of registered courses as created
+    // by Workday
+    // EFFECTS: Convert CSV file into a list of courses and adds courses to Calendar
     public void addCourses(String filePath) throws FileNotFoundException {
         File csvFile = new File(filePath);
         Scanner csvReader = new Scanner(csvFile);
 
-        for (int i = 0; i < 3; i++) {
-            csvReader.nextLine();
-        }
+        csvReader.nextLine();
 
         while (csvReader.hasNextLine()) {
             String line = csvReader.nextLine();
             String[] rows = line.split(",");
-            if (rows.length > 7) {
-                ReadCSV read = new ReadCSV(rows[4].trim(), rows[7].trim());
+            if (rows.length > 4 && rows[4].trim().length() > 5 && rows[1].trim().length() > 3) {
+                ReadCSV read = new ReadCSV(rows[1].trim(), rows[4].trim());
+                System.out.println(rows[1].trim() + rows[4].trim());
                 read.convert();
             }
         }
         csvReader.close();
     }
-    
-    //MODIFIES: this
-    //EFFECTS: changes Calendar name to given name
+
+    // MODIFIES: this
+    // EFFECTS: changes Calendar name to given name
     public void changeName(String name) {
         this.name = name;
     }
--- src/main/ui/CalendarApp.java
+++ src/main/ui/CalendarApp.java
@@ -34,38 +34,58 @@ public class CalendarApp {
     // MODIFIES: this
     // EFFECTS: prompts user to enter CSV file
     public void getCSV() {
-        System.out.println(
-                "Please enter the CSV's file path. If you need help, enter \"HELP\". If you do not want to upload a CSV, enter \"NEW\":");
-        String input = userInput.nextLine();
-        if (input.equals("HELP")) {
-            System.out.println("help text");
-        } else if (input.equals("NEW")) {
-        } else {
-            try {
-                calendar.addCourses(input);
-            } catch (FileNotFoundException e) {
-                System.out.println("Invalid file path");
+        boolean validInput = false;
+        do {
+            System.out.print(
+                    "Please enter the CSV's file path. If you need help, enter \"HELP\". If you do not want to upload a CSV, enter \"NEW\":");
+            String input = userInput.nextLine();
+            if (input.equals("HELP")) {
+                System.out.println("To download the CSV file of your courses, log in to Workday. Go to Academics -> Registration and Courses.");
+                System.out.println("Scroll down to the bottom, where there should be a box with the label \"Current Classes\". On the right, there is a gear icon.");
+                System.out.println("Press the gear icon, then press \"Download to Excel\". Download the file.");
+                System.out.println("Open the file in a spreadsheets application, such as Excel or Google Sheets. Navigate to \"Save As\" or \"Download\", and save the file as an CSV file.");
+                System.out.println("Alternatively, you can Google \"XLSX to CSV\" to find an online converter that can convert the file into a CSV file.");
+                System.out.println("Once the file is downloaded, find it and right click -> Copy as Path.");
+                System.out.println("The copied string will have quotation marks on each side, you must remove these."); 
+                System.out.println("Once this is complete, you can paste the file path into this program!");
+            } else if (input.equals("NEW")) {
+                validInput = true;
+            } else {
+                try {
+                    calendar.addCourses(input);
+                    validInput = true;
+                } catch (FileNotFoundException e) {
+                    System.out.println("Invalid file path");
+                }
             }
-        }
+        } while (!validInput);
     }
 
     // MODIFIES: this
     // EFFECTS: displays available commands and gets next input from user
     public void runCalendar() {
         displayCalendar();
-        System.out.println("Enter a number between 1-7 for each day of the week (1 -> Monday, 7 -> Sunday.)");
+        System.out.println("Enter a number between 1-5 for each day of the week (1 -> Monday, 7 -> Friday).");
         System.out.println("Enter \"r\" to remove an item.");
         System.out.println("Enter \"n\" to add a new item.");
-        System.out.println("Enter a day of week in numerical form and a time, separated by commas (ex. \"1, 12:00\"), to find what item is at that time.");
+        System.out.println("Enter the name of an item to edit it.");
+        System.out.println("Enter \"c\" to change the name of the calendar.");
+        System.out.println(
+                "Enter a day of week in numerical form and a time, separated by commas (ex. \"1, 12:00\"), to find what item is at that time.");
+                System.out.println("Enter \"q\" to quit.");
         String nextCommand = userInput.nextLine();
         processCommand(nextCommand);
     }
 
-    //MODIFIES: this
-    //EFFECTS: processes input from user
+    // MODIFIES: this
+    // EFFECTS: processes input from user
     public void processCommand(String command) {
-        if (Integer.valueOf(command) >= 1 && Integer.valueOf(command) <= 7) {
-        }
+        if (command.equals("q")) {
+            System.exit(0);
+        } else if (Integer.valueOf(command) >= 1 && Integer.valueOf(command) <= 5) {
+            currentDay = Calendar.daysOfWeek.get(Integer.valueOf(command) - 1);
+        } 
+        runCalendar();
     }
 
     // EFFECTS: displays the calendar in the console
@@ -79,7 +99,8 @@ public class CalendarApp {
         System.out.println();
         System.out.println("|     Event Name     |     Event Time     |     Event Location     |");
         for (CalendarItem item : currentDay.getItems()) {
-            System.out.println(String.format("| %-18s | %-8s - %7s | %-22s |", item.getName(), item.getStartTime(), item.getEndTime(), item.getLocation()));
+            System.out.println(String.format("| %-18s | %-8s - %7s | %-22s |", item.getName(), item.getStartTime(),
+                    item.getEndTime(), item.getLocation()));
         }
     }
 
